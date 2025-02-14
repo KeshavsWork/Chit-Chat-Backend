4262b7ed2c869bfb54dfb1843d4947b55c7a5421
KeshavBagad <keshavbagad01@gmail.com>
2025-02-15 03:00:54 +0530

    Added secure app.keys to Strapi

--- config/server.ts
+++ config/server.ts
@@ -1,7 +1,7 @@
 export default ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  host: env("HOST", "0.0.0.0"),
+  port: env.int("PORT", 8080),
   app: {
-    keys: env.array('APP_KEYS'),
+    keys: env.array("APP_KEYS", ["e3a5db8e6611671dc50c7bd0c062ef943b3a4481c317bf013e53d16ebf44af87", "e6c78c0400700d67fd9d0443b0b873f39a0b6d9412a54c321e01ef5a0907398a"]),
   },
 });
